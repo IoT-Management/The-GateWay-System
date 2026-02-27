a713a6d77832c0eeafdaa3146c86199ace14c555
Demo User <debian@beaglebone.localdomain>
2026-02-27 18:47:50 +0000

    added url

--- GateWayDetails.py
+++ GateWayDetails.py
@@ -1,4 +1,4 @@
-GateWay_ID=""
-GateWay_Location=""
-server_url=""
-post_auth=""
+GateWay_ID="lab_105@Gt1"
+GateWay_Location="LAB_105"
+server_url="http://192.168.1.7:9000/gatewayabiuhdidw342d"
+post_auth="gt105abdhsjhsjc"
