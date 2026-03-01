abac1fb75739982b53bf8128fd8cd47605ae7f8c
Demo User <debian@beaglebone.localdomain>
2026-03-01 19:08:03 +0000

    temp changes

--- GateWayDetails.py
+++ GateWayDetails.py
@@ -1,4 +1,4 @@
 GateWay_ID="lab_105@Gt1"
 GateWay_Location="LAB_105"
-server_url="http://192.168.1.7:9000/gatewayabiuhdidw342d"
+server_url="https://api.yashkriti.online/gatewayabiuhdidw342d"
 post_auth="gt105abdhsjhsjc"
